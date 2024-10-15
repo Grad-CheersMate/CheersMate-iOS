9295c8ae48707659a0ad8601b3295f5d1a919668
hoon <stitch8971@gachon.ac.kr>
2024-10-15 16:17:59 +0900

    Initial Commit

---
+++ CheersMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheersMate
+//
+//  Created by 재훈 on 10/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CheersMateTests/CheersMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  CheersMateTests.swift
+//  CheersMateTests
+//
+//  Created by 재훈 on 10/15/24.
+//
+
+import Testing
+@testable import CheersMate
+
+struct CheersMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
