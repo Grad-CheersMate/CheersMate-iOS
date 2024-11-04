cba46e0cab56e035a4d06c0d26703a8d51cff8ec
hoon <stitch8971@gachon.ac.kr>
2024-11-04 21:05:11 +0900

    ✨ 이메일 및 비밀번호 찾기 뷰 모델 설계 1. 이메일 찾기 뷰 모델 2. 비밀번호 찾기 뷰 모델 close: #21

--- CheersMate/Presentation/AccountlSearchScene/View/AccountSearchResultView.swift
+++ CheersMate/Presentation/AccountlSearchScene/View/AccountSearchResultView.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-final class EmailSearchSuccessView: UIView {
+final public class AccountSearchResultView: UIView {
     
     // 사용자 안내 레이블
     private let infoLabel: UILabel = {
         let lb = UILabel()
         lb.textColor = UIColor.textColor
-        lb.text = "이메일 찾기에\n성공했어요."
+        lb.text = ""
         lb.setLineSpacing(spacing: 3)
         lb.numberOfLines = 2
         lb.font = UIFont.gmarketSans(size: 25, family: .Medium)
@@ -21,21 +21,21 @@ final class EmailSearchSuccessView: UIView {
         return lb
     }()
     
-    // 이메일 주소 레이블
-    private let emailLabel: UILabel = {
+    // 이메일 주소 또는 전화번호 레이블
+    private let contactLabel: UILabel = {
         let lb = UILabel()
         lb.textColor = UIColor.textColor
-        lb.text = "가입된 이메일 주소"
+        lb.text = ""
         lb.font = UIFont.gmarketSans(size: 14, family: .Medium)
         lb.textAlignment = .left
         return lb
     }()
     
-    // 이메일 입력 창
-    let emailTextField: UITextField = {
+    // 이메일 주소 또는 전화번호 결과 창
+    public var contactTextField: UITextField = {
         let tf = UITextField()
-        tf.font = UIFont.pretendard(size: 16, family: .SemiBold)
-        tf.text = "gachon123@gachon.ac.kr"
+        tf.font = UIFont.pretendard(size: 16, family: .Medium)
+        tf.text = "결과창입니다"
         tf.isEnabled = false
         tf.keyboardType = .emailAddress
         tf.autocapitalizationType = .none
@@ -44,18 +44,18 @@ final class EmailSearchSuccessView: UIView {
         return tf
     }()
     
-    // 이메일 입력 창이 클릭됬을 때 표시하는 언더라인
-    var emailUnderLine: UIView = {
+    // 이메일 주소 또는 전화번호 입력 창이 클릭됬을 때 표시하는 언더라인
+    public var contactUnderLine: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.mainColor
         view.clipsToBounds = true
         return view
     }()
     
-    // 로그인 하기 버튼
-    let logInButton: UIButton = {
+    // 완료 버튼
+    public let completeButton: UIButton = {
         let bt = UIButton(type: .custom)
-        bt.setTitle("로그인 하기", for: .normal)
+        bt.setTitle("", for: .normal)
         bt.setTitleColor(.white, for: .normal)
         bt.titleLabel?.font = UIFont.gmarketSans(size: 17, family: .Medium)
         bt.layer.cornerRadius = 8
@@ -63,13 +63,13 @@ final class EmailSearchSuccessView: UIView {
         return bt
     }()
 
-    override init(frame: CGRect) {
+    public override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
         setupLayout()
     } // closed init
     
-    required init?(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     } // closed init
     
@@ -77,7 +77,7 @@ final class EmailSearchSuccessView: UIView {
     private func setupUI() {
         self.backgroundColor = .white
         
-        [infoLabel, emailLabel, emailTextField, emailUnderLine, logInButton]
+        [infoLabel, contactLabel, contactTextField, contactUnderLine, completeButton]
             .forEach { self.addSubview($0) }
         
     } // closed setupUI
@@ -90,32 +90,54 @@ final class EmailSearchSuccessView: UIView {
             make.centerX.equalToSuperview()
         }
         
-        emailLabel.snp.makeConstraints { make in
+        contactLabel.snp.makeConstraints { make in
             make.top.equalTo(infoLabel.snp.bottom).offset(60)
             make.leading.trailing.equalToSuperview().inset(25)
             make.centerX.equalToSuperview()
         }
         
-        emailTextField.snp.makeConstraints { make in
-            make.top.equalTo(emailLabel.snp.bottom).offset(10)
+        contactTextField.snp.makeConstraints { make in
+            make.top.equalTo(contactLabel.snp.bottom).offset(10)
             make.leading.trailing.equalToSuperview().inset(25)
             make.centerX.equalToSuperview()
             make.height.equalTo(25)
         }
         
-        emailUnderLine.snp.makeConstraints { make in
-            make.top.equalTo(emailTextField.snp.bottom).offset(10)
+        contactUnderLine.snp.makeConstraints { make in
+            make.top.equalTo(contactTextField.snp.bottom).offset(10)
             make.leading.trailing.equalToSuperview().inset(25)
             make.centerX.equalToSuperview()
             make.height.equalTo(1)
         }
         
-        logInButton.snp.makeConstraints { make in
-            make.top.equalTo(emailUnderLine.snp.bottom).offset(100)
+        completeButton.snp.makeConstraints { make in
+            make.top.equalTo(contactUnderLine.snp.bottom).offset(100)
             make.leading.trailing.equalToSuperview().inset(25)
             make.centerX.equalToSuperview()
             make.height.equalTo(45)
         }
     } // closed setupLayout
+    
+    public func configure(viewType: ViewType, outcome: Outcome) {
+        switch (viewType, outcome) {
+        case (.searchEmail, .success):
+            self.infoLabel.text = "이메일 찾기에\n성공했어요."
+            self.contactLabel.text = "이메일 주소"
+            self.completeButton.setTitle("로그인하기", for: .normal)
+        case (.searchEmail, .failure):
+            self.infoLabel.text = "이메일 찾기에\n실패했어요."
+            self.contactLabel.text = "이메일 주소"
+            self.completeButton.setTitle("뒤로가기", for: .normal)
+        case (.searchPassword, .success):
+            self.infoLabel.text = "임시 비밀번호를\n전송했어요."
+            self.contactLabel.text = "휴대폰 번호"
+            self.completeButton.setTitle("로그인하기", for: .normal)
+        case (.searchPassword, .failure):
+            self.infoLabel.text = "비밀번호 찾기에\n실패했어요."
+            self.contactLabel.text = "휴대폰 번호"
+            self.completeButton.setTitle("뒤로가기", for: .normal)
+        }
+
+    } // closed config
     
 } // closed Class
---
+++ CheersMate/Presentation/AccountlSearchScene/ViewModel/AccountSearchViewModel.swift
@@ -0,0 +1,89 @@
+//
+//  EmailSearchViewModel.swift
+//  CheersMate
+//
+//  Created by 재훈 on 11/4/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+public protocol AccountSearchViewModelProtocol {
+    func transform(input: AccountSearchViewModel.Input) -> AccountSearchViewModel.Output
+}
+
+final public class AccountSearchViewModel: AccountSearchViewModelProtocol {
+    private let useCase: UserUseCaseProtocol
+    private let isValidTextRelay = BehaviorRelay<Bool>(value: false) // 이메일, 비밀번호, 전화번호가 모두 유효한지 체크
+    private let responseRelay = PublishRelay<UserResponse>()
+    private let errorRelay = PublishRelay<Error>()
+    private let disposeBag: DisposeBag = DisposeBag()
+    
+    public init(useCase: UserUseCaseProtocol) {
+        self.useCase = useCase
+    } // closed init
+    
+    public struct Input {
+        let contactTextField: Driver<String> // 닉네임 또는 이메일 입력 문자열
+        let tellTextField: Driver<String> // 전화번호 입력 문자열
+        let contactSearchButtonTapped: ControlEvent<Void> // 닉네임 또는 이메일 찾기 버튼 클릭 이벤트
+        let viewType: ViewType // 이메일 찾기와 비밀번호 찾기를 구분하기 위한 타입
+    } // closed Input
+    
+    public struct Output {
+        let contactSearchButtonEnabled: Driver<Bool> // 닉네임 또는 이메일 찾기 버튼의 활성화
+        let contactSearchResponse: Signal<UserResponse> // 닉네임 또는 이메일 찾기 버튼 요청 응답
+        let contactSearchError: Signal<Error> // 닉네임 또는 이메일 찾기 버튼 요청 에러
+    } // closed Output
+    
+    public func transform(input: Input) -> Output {
+        
+        Driver.combineLatest(
+            input.contactTextField,
+            input.tellTextField)
+        .map { [weak self] contact, tell in
+            switch input.viewType {
+            case .searchEmail:
+                (self?.useCase.isMatchingRegex(text: contact, type: .nickname) ?? false) &&
+                (self?.useCase.isMatchingRegex(text: tell, type: .tell) ?? false)
+            case .searchPassword:
+                (self?.useCase.isMatchingRegex(text: contact, type: .email) ?? false) &&
+                (self?.useCase.isMatchingRegex(text: tell, type: .tell) ?? false)
+            }
+        }
+        .drive(isValidTextRelay)
+        .disposed(by: disposeBag)
+        
+        // flatMapLatest는 내부 옵저버블을 구독하는데 이때 에러가 발생하면 스트림이 끊어지니 주의할 것!
+        input.contactSearchButtonTapped
+            .withLatestFrom(Observable.combineLatest(input.contactTextField.asObservable(),
+                                                     input.tellTextField.asObservable(),
+                                                     isValidTextRelay))
+            .filter { $2 }
+            .flatMapLatest { [weak self] contact, tell, _ -> Single<UserResponse> in
+                guard let self = self else { return Single.never() }
+                switch input.viewType {
+                case .searchEmail:
+                    return self.useCase.searchEmail(nickname: contact, tell: tell)
+                        .catch { [weak self] err in
+                            self?.errorRelay.accept(err)
+                            return Single.never()
+                        }
+                case .searchPassword:
+                    return self.useCase.searchPassword(email: contact, tell: tell)
+                        .catch { [weak self] err in
+                            self?.errorRelay.accept(err)
+                            return Single.never()
+                        }
+                }
+            }
+            .subscribe( onNext: { [weak self] userResponse in
+                self?.responseRelay.accept(userResponse)})
+            .disposed(by: disposeBag)
+        
+        
+        return Output(contactSearchButtonEnabled: isValidTextRelay.asDriver(), contactSearchResponse: responseRelay.asSignal(), contactSearchError: errorRelay.asSignal())
+    } // closed transform
+    
+} // closed SignUpViewModel
